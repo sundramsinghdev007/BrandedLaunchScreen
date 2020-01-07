c7389bd35fac447322f45cb03f75c7596d28c162
Annu1Singh <32551778+Annu1Singh@users.noreply.github.com>
2020-01-07 21:49:20 +0530

    final

--- app/src/main/java/com/sundram/brandedlaunchscreen/SplashActivity.java
+++ app/src/main/java/com/sundram/brandedlaunchscreen/SplashActivity.java
@@ -33,6 +33,6 @@ public class SplashActivity extends AppCompatActivity {
                 overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
                 finish();
             }
-        },8000);
+        },4000);
     }
 }
